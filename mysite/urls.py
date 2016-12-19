55e878ebc5d6613c10570bb7344e20f57361a8c4
Shubham <shubhamim3@gmail.com>
2016-12-19 15:45:30 +0530

    Updated features for security and comments

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
-
+import django.contrib.auth.views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', django.contrib.auth.views.login, name= 'login'),
+    url(r'^login/$', django.contrib.auth.views.login, name ='login'),    
+    url(r'logout', django.contrib.auth.views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
+    
     
     
 ]
